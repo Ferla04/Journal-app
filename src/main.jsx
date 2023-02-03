b5e873164e7d8f664b8dbe1a836cc3e2e7d8157b
Ferla04 <fer.velas135@gmail.com>
2023-02-02 19:14:52 -0500

    vercel config

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './store'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import { JournalApp } from './JournalApp'
 import './styles.css'
@@ -10,9 +10,9 @@ import './styles.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={ store }>
-      <HashRouter>
+      <BrowserRouter>
         <JournalApp />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 )
